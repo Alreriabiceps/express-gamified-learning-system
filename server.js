30d2b7a24fe5db079dba371c7273bfb73b837503
russelle roxas <russelleroxas11@gmail.com>
2025-04-27 02:15:29 +0800

    try

--- server.js
+++ server.js
@@ -44,7 +44,11 @@ if (!MONGO_URI) {
 // Apply middleware to handle CORS and JSON requests
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://vite-gamified-learning-system.vercel.app'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
