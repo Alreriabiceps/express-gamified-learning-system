897fc6bc5c43d3a1049bd3d50c2b79fe13a6db4c
russelle roxas <russelleroxas11@gmail.com>
2025-05-06 02:36:46 +0800

    try

--- config/config.js
+++ config/config.js
@@ -13,6 +13,7 @@ module.exports = {
         'http://127.0.0.1:5173',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'https://alreria.vercel.app',
         undefined // Allow undefined origin for local development
     ],
 
